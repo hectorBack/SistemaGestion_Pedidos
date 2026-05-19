d55ccdb7f5ba9412842964f616aba0dc7c3338e1
hectorBack <hectorSalvador2001@hotmail.com>
2026-05-18 19:38:50 -0600

    feat: cambio en PedidoRequestDTO agregando una validacion en el atributo nombreCliente

--- src/main/java/com/Sistema/Backend/Dto/Request/PedidoRequestDTO.java
+++ src/main/java/com/Sistema/Backend/Dto/Request/PedidoRequestDTO.java
@@ -14,6 +14,7 @@ public class PedidoRequestDTO {
     @Size(min = 4, max = 4, message = "Deben ser exactamente los últimos 4 dígitos")
     private String whatsappFinal;
 
+    @NotBlank(message = "El nombre del cliente es obligatorio")
     private String nombreCliente;
 
     @NotEmpty(message = "El pedido debe tener al menos un producto")
